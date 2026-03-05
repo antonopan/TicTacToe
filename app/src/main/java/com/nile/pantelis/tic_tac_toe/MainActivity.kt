3f001ed2cc93fa25bb05ec133506b7cc130a8be0
pantelis <pantelisanonopoulos11@gmail.com>
2026-03-05 17:33:12 +0200

    Initial commit

---
+++ app/src/main/java/com/nile/pantelis/tic_tac_toe/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.nile.pantelis.tic_tac_toe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nile.pantelis.tic_tac_toe.ui.theme.TicTacToeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TicTacToeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
